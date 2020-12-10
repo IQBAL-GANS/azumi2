48afcb0cc1a0d76bdf284e41a95156535fd1888f
IQBAL-GANS <74524617+IQBAL-GANS@users.noreply.github.com>
2020-12-10 13:53:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fetch = require('node-fetch');
 const urlencode = require("urlencode");
 const axios = require("axios");
 const menu = require("./lib/menu.js");
-const donasi = require("./lib/donasi.js");
+const donasi = require(".lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
 const BotName = 'Azumi[BOT]'; 
